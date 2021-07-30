54b4beda2ec7472dea4ee07651fe9417e5d15a09
Jonathon Wilks <jonathondwilks@icloud.com>
2021-07-30 11:56:07 +0100

    saving as re-doing in typescript - its not pretty but it works !

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import "./App.css";
 import Form from "./components/Form";
 import TodosList from "./components/TodosList";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Test from "./Pages/Test";
+
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <Form />
-      </header>
-      <main>
-        <TodosList />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" exact>
+          <Form />
+        </Route>
+        <Route path="/" exact>
+          <TodosList />
+        </Route>
+        <Route path="/test">
+          <Test />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Pages/Test.js
@@ -0,0 +1,10 @@
+function Test() {
+  return (
+    <div>
+      <h1>THIS IS A TEST PAGE</h1>
+      <h2>Testing Routes</h2>
+    </div>
+  );
+}
+
+export default Test;
